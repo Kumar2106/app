11c8c1f879e25ee4710a44dc4c8e7b1291fafe4b
Kumar Aditya <ka09934147002@gmail.com>
2020-06-22 15:10:21 +0530

    Added model for the Video Details

---
+++ app/src/main/java/com/example/myapplication/model/VideoDetails.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.model
+
+import java.util.*
+
+class VideoDetails(
+    val videoId:Int,
+    val headline:String,
+    val videoUrl:String,
+    val createdDate:Date,
+    val lastUpdateDate:Date
+
+)
